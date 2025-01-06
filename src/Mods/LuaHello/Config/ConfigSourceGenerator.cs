4c7cd8ac32404d1f5d591c1a9043e55d4a440404
JustArion <53127594+JustArion@users.noreply.github.com>
2025-01-06 18:37:12 +0100

    Remove mod config

--- src/Mods/LuaHello/Config/ConfigSourceGenerator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Dawn.DarkCrusade.Mods.BorderlessWindowed.Config;
-
-using System.Text.Json;
-using System.Text.Json.Serialization;
-
-[JsonSerializable(typeof(ModConfig))]
-[JsonSourceGenerationOptions(WriteIndented = true, IncludeFields = true)]
-public partial class ConfigSourceGenerator : JsonSerializerContext;
--- src/Mods/LuaHello/Config/ModConfig.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Dawn.DarkCrusade.Mods.BorderlessWindowed.Config;
-
-public record ModConfig
-{
-    public bool Enabled = true;
-}
